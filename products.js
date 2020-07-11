596b4ffef74b8bcd30fa8880f7a7cb174529ab4a
mprokhorov <proch-m@yandex.ru>
2020-07-11 23:19:31 +0300

    collect products h1

--- products.js
+++ products.js
@@ -19,35 +19,72 @@ const ProductsList = db.productsList;
 
 (async () => {
     const pages = [];
+    const configs = [];
+    const getPageObject = (pageObject) => {
+        console.log(pageObject);
+        pages.push(pageObject);
+    };
     const config = {
         baseSiteUrl: args.url,
         startUrl: args.url,
         filePath: './htdocs/images/',
         concurrency: 10,
         maxRetries: 3,
+        timeout: 180000,
+        delay: 1200,
         logPath: './logs/'
     };
 
+    let resCategories = [];
     await Category.findAll({
         raw: true,
-        attributes: ['title', 'url']
-    }).then((category) => {
-        console.log(category);
+        attributes: ['title', 'url', 'id']
+    }).then((categories) => {
+        resCategories = categories;
+    }).catch((reason) => {
+        console.error(reason);
     });
+
+    await (() => {
+        resCategories.forEach((category) => {
+            if (!! category.url) {
+                config.baseSiteUrl = category.url;
+                config.startUrl = category.url;
+                configs.push(config);
+            }
+        });
+    })();
+
+    await (() => {
+        configs.forEach((configItem) => {
+            const scraper = new Scraper(configItem);
+            const root = new Root({
+                pagination: {
+                    queryString: 'page',
+                    begin: 1,
+                    end: 5
+                }
+            });
+            const jobAds = new OpenLinks('#product-list-grid .product-block .name a', {
+                name: 'Product page',
+                getPageObject
+            });
+            const title = new CollectContent('h1', {name: 'title'});
+            root.addOperation(jobAds);
+            jobAds.addOperation(title);
+
+            scraper.scrape(root);
+            fs.appendFile(
+                'results/products.json',
+                JSON.stringify(pages),
+                (err) => {
+                    console.error(err);
+                });
+        });
+    })();
 
     /*
-    const getPageObject = (pageObject) => {
-        pages.push(pageObject);
-    };
 
-    const scraper = new Scraper(config);
-    const root = new Root({
-        pagination: {
-            queryString: '.pagination .links a',
-            begin: 1,
-            end: 50
-        }
-    });
     const product = new CollectContent('#product-list-grid li', {
         name: '.product-block .name'
     });
